90ac5d7f5e847f98c9b5d3c0ccfdd42aa89d0a83
Jared Sprague <jared@caramelcode.com>
2016-09-02 22:41:53 -0400

    Finished

--- linode-near-location.js
+++ linode-near-location.js
@@ -1,6 +1,4 @@
 function linodeNearLocation() {
-    console.log('tzo', (new Date()).getTimezoneOffset());
-
     // Add Daylight Savings Time detection, all most all dst countries are northern hemisphere
     // http://javascript.about.com/library/bldst.htm
     Date.prototype.stdTimezoneOffset = function() {
@@ -13,7 +11,58 @@ function linodeNearLocation() {
     };
 
     var today = new Date();
-    if (today.dst()) { console.log("Daylight savings time!"); }
+    var timezoneOffset = today.getTimezoneOffset() / 60;
+    var linode_location;
 
-    return 'foo';
+    if (today.dst()) {
+        timezoneOffset++;  // add one if we are currently in daylight savings time
+    }
+
+    // convert to actual value because signs are reversed in getTimezoneOffset
+    timezoneOffset = Math.round(0 - timezoneOffset);
+
+    // refer to this picture: https://cloud.githubusercontent.com/assets/3926730/18216404/7a3862a4-7124-11e6-91a1-0d03b2c7f9d5.gif
+    switch (timezoneOffset) {
+        case -12:
+        case -11:
+        case -10:
+        case -9:
+        case -8:
+            linode_location = 'fremont';
+            break;
+        case -7:
+        case -6:
+            linode_location = 'dallas';
+            break;
+        case -5:
+        case -4:
+        case -3:
+            linode_location = 'newark';
+            break;
+        case -2:
+        case -1:
+        case 0:
+            linode_location = 'london';
+            break;
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+            linode_location = 'frankfurt';
+            break;
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+            linode_location = 'singapore';
+            break;
+        default:
+            linode_location = 'newark';
+    }
+
+    return linode_location;
 }
